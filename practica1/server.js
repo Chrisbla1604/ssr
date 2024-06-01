d5b122b75279ebe5fec469179225e9262a0c7a05
Chrisbla1604 <chrisbla160487@gmail.com>
2024-06-01 02:08:42 -0500

    commit practica 1

---
+++ practica1/server.js
@@ -0,0 +1,14 @@
+ 
+ 
+ // server.js
+ const express = require('express');
+ const app = express();
+ const port = 3000;
+
+ app.get('/', (req, res) => {
+     res.send('<h1>Hello Christian Blanco from Server Side Rendering!</h1>');
+ });
+
+ app.listen(port, () => {
+     console.log(`Server is running on http://localhost:${port}`);
+ });
