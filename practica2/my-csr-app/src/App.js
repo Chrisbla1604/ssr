234889e59cafa42918dc4168e5324808b760fd90
Chrisbla1604 <chrisbla160487@gmail.com>
2024-06-01 02:53:34 -0500

    commit practica 2 y 3

---
+++ practica2/my-csr-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <h1>Hello Christian from Client Side Rendering!</h1>
+    </div>
+  );
+}
+
+export default App;
